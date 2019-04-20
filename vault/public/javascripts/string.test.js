884500623b8b51ec9641a1bde3ca6a91ca1bab3f
Frederik Ring <frederik.ring@gmail.com>
2019-04-20 08:06:06 -0700

    use budo for vault development

--- vault/public/javascripts/string.test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert')
-
-const string = require('./string')
-
-describe('string', function () {
-  it('exports a string', function () {
-    assert(typeof string === 'string')
-  })
-})
--- vault/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/', function (req, res, next) { // eslint-disable-line no-unused-vars
-  res.render('index', { title: 'Offen' })
-})
-
-module.exports = router
