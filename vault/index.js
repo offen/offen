d14b4b483445865bb86f5264b968945c6433c3de
Frederik Ring <frederik.ring@gmail.com>
2019-05-01 20:34:00 -0600

    refer to real world .dev domain

--- vault/index.js
+++ vault/index.js
@@ -1,6 +1,6 @@
 const ensureUserSecret = require('./src/user-secret')
 
-ensureUserSecret('9b63c4d8-65c0-438c-9d30-cc4b01173393', 'https://local.offen.org:8080')
+ensureUserSecret('9b63c4d8-65c0-438c-9d30-cc4b01173393', 'https://local.offen.dev:8080')
   .then(function (userSecret) {
     console.log(userSecret)
   })
