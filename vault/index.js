2d27fb749ca88f5538f60dca70431c8b05430b4d
Frederik Ring <frederik.ring@gmail.com>
2019-05-08 17:19:54 +0200

    add basic post message interface and relay encrypted payload

--- vault/index.js
+++ vault/index.js
@@ -1,9 +1,59 @@
 const ensureUserSecret = require('./src/user-secret')
 
-ensureUserSecret('9b63c4d8-65c0-438c-9d30-cc4b01173393', 'https://local.offen.dev:8080')
-  .then(function (userSecret) {
-    console.log(userSecret)
-  })
-  .catch(function (err) {
-    console.error(err)
-  })
+window.addEventListener('message', function (event) {
+  let payload
+  try {
+    payload = JSON.parse(event.data)
+  } catch (err) {
+    console.warn('Received malformed event, skipping.')
+    return
+  }
+
+  ensureUserSecret(payload.accountId, process.env.SERVER_HOST)
+    .then(function (userSecret) {
+      const enc = new TextEncoder()
+      return window.crypto.subtle.encrypt({
+        name: 'AES-CTR',
+        counter: new Uint8Array(16),
+        length: 128
+      },
+      userSecret,
+      enc.encode(JSON.stringify(payload.event)))
+    })
+    .then(function (encrypted) {
+      const encryptedEventPayload = arrayBufferToBase64(encrypted)
+      return window
+        .fetch(`${process.env.SERVER_HOST}/events`, {
+          method: 'POST',
+          credentials: 'include',
+          body: JSON.stringify({
+            account_id: payload.accountId,
+            payload: encryptedEventPayload
+          })
+        })
+        .then(function (response) {
+          if (response.status >= 400) {
+            return response.json().then(function (errorBody) {
+              const err = new Error(errorBody.error)
+              err.status = response.status
+              throw err
+            })
+          }
+          return response.json()
+        })
+    })
+    .then(function (result) {
+      console.log(result)
+    })
+    .catch(function (err) {
+      console.error(err)
+    })
+})
+
+function arrayBufferToBase64 (byteArray) {
+  const chars = Array.from(new Uint8Array(byteArray))
+    .map(function (byte) {
+      return String.fromCharCode(byte)
+    })
+  return window.btoa(chars.join(''))
+}
