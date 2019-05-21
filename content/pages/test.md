6b72b26b3c0ee7e98d06c026c52973d23f1da3df
Hendr_ik <hello@niefeld.com>
2019-05-21 23:01:57 +0200

    transfer old page

--- content/pages/test.md
+++ content/pages/test.md
@@ -1,7 +1,3 @@
-Title: A markdown powered test page
-Date: 2019-05-21
+Title: Test Page
 
 This is a test page.
-
-[a root-relative link to unbelievable](|filename|/unbelievable.rst)
-[a file-relative link to unbelievable](|filename|../unbelievable.rst)
--- pelicanconf.py
+++ pelicanconf.py
@@ -3,12 +3,12 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'offen'
-SITENAME = 'offen home page'
+SITENAME = 'offen'
 SITEURL = ''
 
 PATH = 'content'
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Europe/Berlin'
 
 DEFAULT_LANG = 'en'
 
@@ -19,17 +19,10 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
+# pagination
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+THEME = 'offen'
