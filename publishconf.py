d0f0a01389fb1d08c637aedb28f0bbebaaf7f8ec
Frederik Ring <frederik.ring@gmail.com>
2019-06-01 16:06:24 +0200

    update production SITEURL

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://www.offen.dev/homepage'
+SITEURL = 'https://www.offen.dev'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
