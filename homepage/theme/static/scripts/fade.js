674738720d6015b020e28aebf45fc1e5c62a1826
Frederik Ring <frederik.ring@gmail.com>
2019-08-30 19:20:07 +0200

    add body class for propagating chosen template

--- homepage/theme/static/scripts/fade.js
+++ homepage/theme/static/scripts/fade.js
@@ -2,7 +2,7 @@
   $(document).ready(function () {
     $(window).scroll(function () {
       var scrollProgress = parseInt($(window).scrollTop(), 10)
-      $('.brand-index').css('opacity', Math.min(scrollProgress / 100, 1))
+      $('body.index .brand').css('opacity', Math.min(scrollProgress / 100, 1))
     })
   })
 })(window.jQuery)
