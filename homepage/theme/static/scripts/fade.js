58cfa301e517a7862fcef49583df53029c58a217
hendr-ik <hello@niefeld.com>
2019-09-08 20:51:51 +0200

    update svg css fade.js

--- homepage/theme/static/scripts/fade.js
+++ homepage/theme/static/scripts/fade.js
@@ -2,7 +2,9 @@
   $(document).ready(function () {
     $(window).scroll(function () {
       var scrollProgress = parseInt($(window).scrollTop(), 10)
-      $('body.index .brand').css('opacity', Math.min(scrollProgress / 100, 1))
+        if ($(window).width() > 960) {
+          $('body.index .brand').css('opacity', Math.min(scrollProgress / 100, 1))
+        }
     })
   })
 })(window.jQuery)
