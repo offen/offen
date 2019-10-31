bd3f021bfdb74d8f7805b354c7da6ce33f319115
Frederik Ring <frederik.ring@gmail.com>
2019-10-31 11:44:05 +0100

    introduce root account concept, enable dynamic template rendering

---
+++ server/router/root.go
@@ -0,0 +1,13 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (rt *router) getRoot(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.go.html", map[string]interface{}{
+		"rootAccount": rt.settings.rootAccount,
+	})
+}
