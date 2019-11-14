8be764f2006ea770be367a2e11eaedfbd08a29dd
Frederik Ring <frederik.ring@gmail.com>
2019-11-14 10:53:20 +0100

    add tests for database transactions

--- server/persistence/relational/relational_test.go
+++ server/persistence/relational/relational_test.go
@@ -1,6 +1,8 @@
 package relational
 
 import (
+	"testing"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
@@ -19,3 +21,46 @@ func createTestDatabase() (*gorm.DB, func() error) {
 type dbAccess func(*gorm.DB) error
 
 var noop dbAccess = func(*gorm.DB) error { return nil }
+
+func TestRelationalDAL_Ping(t *testing.T) {
+	db, closeDB := createTestDatabase()
+	defer closeDB()
+
+	dal := NewRelationalDAL(db)
+	if err := dal.Ping(); err != nil {
+		t.Errorf("Unexpected error pinging database: %v", err)
+	}
+}
+
+func TestRelationalDAL_DropAll(t *testing.T) {
+	db, closeDB := createTestDatabase()
+	defer closeDB()
+
+	if err := db.Save(&Event{
+		EventID: "event-id",
+		Payload: "payload",
+	}).Error; err != nil {
+		t.Fatalf("Unexpected error setting up test: %v", err)
+	}
+
+	dal := NewRelationalDAL(db)
+	if err := dal.DropAll(); err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	err := db.Where("event_id = ?", "event-id").First(&Event{}).Error
+	if !gorm.IsRecordNotFoundError(err) {
+		t.Errorf("Unexpected error value %v", err)
+	}
+}
+
+func TestRelationalDAL_ApplyMigrations(t *testing.T) {
+	db, closeDB := createTestDatabase()
+	defer closeDB()
+
+	dal := NewRelationalDAL(db)
+
+	if err := dal.ApplyMigrations(); err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+}
