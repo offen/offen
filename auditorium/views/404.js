d06396292714b777241cfd64b7a21106c449aa8f
hendr-ik <hello@niefeld.com>
2019-11-17 08:26:07 +0100

    circle ci test

--- auditorium/views/404.js
+++ auditorium/views/404.js
@@ -4,7 +4,7 @@ module.exports = view
 
 function view (state, emit) {
   var notFoundMessage = html`
-    <p class="error dib pa2 black br2 bg-black-05 mt0 mb2">${__('Not found...')}</p>
+    <p data-role="temp" class="error dib pa2 black br2 bg-black-05 mt0 mb2">${__('Not found...')}</p>
   `
   return notFoundMessage
 }
