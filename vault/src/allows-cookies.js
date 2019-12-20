b1aa1fa3f4b055212e671c799beaf57b78382493
Frederik Ring <frederik.ring@gmail.com>
2019-12-20 09:26:50 +0100

    always specify SameSite value for cookies

--- vault/src/allows-cookies.js
+++ vault/src/allows-cookies.js
@@ -1,9 +1,25 @@
 module.exports = allowsCookies
 
 function allowsCookies () {
-  var token = Math.random().toString(16).slice(2)
-  document.cookie = 'ok=' + token
+  var token = randomString()
+  document.cookie = serialize({
+    ok: token, SameSite: 'Lax'
+  })
   var support = document.cookie.indexOf(token) >= 0
-  document.cookie = 'ok=; expires=' + new Date(0).toUTCString()
+  document.cookie = serialize({
+    ok: '', expires: new Date(0).toUTCString(), SameSite: 'Lax'
+  })
   return support
+}
+
+function serialize (obj) {
+  return Object.keys(obj)
+    .map(function (key) {
+      return key + '=' + obj[key]
+    })
+    .join(';')
+}
+
+function randomString () {
+  return Math.random().toString(16).slice(2)
 }
