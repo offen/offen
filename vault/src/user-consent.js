deaa4663f52ca9bb595ad0e6117ee638c46535d5
hendr-ik <hello@niefeld.com>
2020-01-05 09:26:17 +0100

    layout desktop

--- vault/src/user-consent.js
+++ vault/src/user-consent.js
@@ -12,20 +12,29 @@ exports.askForConsent = askForConsent
 function askForConsent () {
   return new Promise(function (resolve) {
     var banner = html`
-      <div class="roboto dark-gray bg-white absolute w-100 h-100 ph5 pv2 flex flex-column items-center justify-center">
-        <div>
-          <p class="mt0">
-            ${__('Are you ok with us collecting usage data?')}
+      <div class="w-100 h-100 absolute roboto f5 lh-copy dark-gray bg-white">
+        <div class="ma4-ns ma3">
+          <p class="mt0 mb3 b">
+            ${__('Continue with transparent analytics')}
           </p>
-          <button class="pointer f5 tc dim bn ph3 pv2 dib br1 white bg-dark-green mr2" onclick=${handleConsentAction(ALLOW)}>
-            ${__('Accept')}
-          </button>
-          <button class="pointer f5 tc dim bn ph3 pv2 dib br1 white bg-dark-green mr2" onclick=${handleConsentAction(DENY)}>
-            ${__('Decline')}
-          </button>
-          <a target="_blank" rel="noopener" href="https://www.offen.dev" class="f5 tc dim bn ph3 pv2 dib br1 white bg-dark-green">
-            ${__('Learn more')}
-          </button>
+          <p class="mt0 mb4">
+            ${__('Help us to make this website better by granting access to your usage data. Your data always remains yours. Review and delete it at any time.')}
+            <a target="_blank" rel="noopener" href="https://www.offen.dev" class="link underline dim dark-gray ">
+              ${__('Learn more')}
+            </a>
+          </p>
+          <div class="flex pb5">
+            <div class="w-50 mr2">
+              <button class="w-100 pointer f5 tc dim bn ph3 pv2 dib br1 white bg-dark-gray" onclick=${handleConsentAction(ALLOW)}>
+                ${__('Yes Please')}
+              </button>
+            </div>
+            <div class="w-50 ml2">
+              <button class="w-100 pointer f5 tc dim bn ph3 pv2 dib br1 white bg-dark-gray" onclick=${handleConsentAction(DENY)}>
+                ${__('I Do Not Allow')}
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     `
@@ -54,15 +63,17 @@ function hostStyles (selector) {
   ${selector} {
     display: block !important;
     position: fixed;
+    top: 50vh;
     right: 0;
-    bottom: 0;
     left: 0;
-    height: 120px;
-    width: 100%;
+    margin: 0 auto;
+    height: 220px;
+    width: 450px;
   }
   @media all and (max-width: 480px) {
     ${selector} {
-      height: 25vh;
+      height: 250px;
+      width: 92vw;
     }
   }
 </style>`,
