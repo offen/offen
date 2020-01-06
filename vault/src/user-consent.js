449204a491a4f2ec9fb176a228f8333c0dbe33fe
Frederik Ring <frederik.ring@gmail.com>
2020-01-06 16:43:17 +0100

    enable iframed banner to change positioning at runtime

--- vault/src/user-consent.js
+++ vault/src/user-consent.js
@@ -9,96 +9,164 @@ exports.DENY = DENY
 
 exports.askForConsent = askForConsent
 
-function askForConsent () {
+function askForConsent (styleHost) {
   return new Promise(function (resolve) {
-    var banner = html`
+    var isCollapsed = false
+    var stylesReady = new Promise(function (resolve) {
+      var styleSheet = html`
+        <link rel="stylesheet" href="/fonts.css">
+        <link rel="stylesheet" href="/tachyons.min.css" onload="${resolve}">
+        ${bannerStyles()}
+      `
+      document.head.appendChild(styleSheet)
+    })
 
+    window.addEventListener('resize', onResize)
+    render()
 
-    <div class="roboto f5-ns f6 shadow-2 bg-white">
+    function handleCollapseAction () {
+      isCollapsed = !isCollapsed
+      render()
+    }
 
-    <input id="collaps" class="toggle" type="checkbox" checked>
-    
-    <label for="collaps" class="lbl-toggle"></label>
-    
-    <div class="collaps-content">
-      <p class="b mt0 mb3 mh3">
-        ${__('Continue with transparent analytics')} 
-        <a target="_blank" rel="noopener" href="https://www.offen.dev" class="normal link underline dim dark-gray" id="toggle-link">
-          ${__('Learn more')}
-        </a>
-      </p>
-      <p class="mt0 mb3 mh3" id="toggle-text">
-        ${__('Help us to make this website better by granting access to your usage data. Your data always remains yours. Review and delete it at any time.')}
-        <a target="_blank" rel="noopener" href="https://www.offen.dev" class="link underline dim dark-gray ">
-          ${__('Learn more')}
-        </a>
-      </p>
-    </div>
-    
-    <div class="flex pb3 mh3">
-      <div class="w-50 mr2">
-        <button class="w-100 pointer f5 tc dim bn ph3 pv2 dib br1 white bg-dark-gray" onclick=${handleConsentAction(ALLOW)}>
-          ${__('Yes Please')}
-        </button>
-      </div>
-      <div class="w-50 ml2">
-        <button class="w-100 pointer f5 tc dim bn ph3 pv2 dib br1 white bg-dark-gray" onclick=${handleConsentAction(DENY)}>
-          ${__('I Do Not Allow')}
-        </button>
-      </div>
-    </div>
-
-    </div>
-      
-      
-    `
-    var styleSheet = html`
-      <link rel="stylesheet" href="/tachyons.min.css">
-      <link rel="stylesheet" href="/fonts.css">
-      <link rel="stylesheet" href="/banner-fix.css">
-    `
-    document.head.appendChild(styleSheet)
-    document.body.appendChild(banner)
-
-    function handleConsentAction (result) {
+    function makeConsentActionHandler (result) {
       return function () {
+        styleHost({
+          styles: hostStylesHidden(styleHost.selector).hidden.innerHTML
+        })
+        while (document.body.firstChild) {
+          document.body.removeChild(document.body.firstChild)
+        }
+        window.removeEventListener('resize', onResize)
         resolve(result)
-        document.body.removeChild(banner)
       }
+    }
+
+    function onResize (event) {
+      adjustHostStyles()
+    }
+
+    function adjustHostStyles (styles) {
+      stylesReady.then(function () {
+        styleHost({
+          attributes: {
+            height: document.body.clientHeight
+          },
+          styles: styles
+        })
+      })
+    }
+
+    function render () {
+      while (document.body.firstChild) {
+        document.body.removeChild(document.body.firstChild)
+      }
+      var banner = bannerView(
+        isCollapsed,
+        handleCollapseAction,
+        makeConsentActionHandler(ALLOW),
+        makeConsentActionHandler(DENY)
+      )
+      document.body.appendChild(banner)
+      adjustHostStyles(
+        hostStylesVisible(styleHost.selector, isCollapsed).innerHTML
+      )
     }
   })
 }
 
-exports.hostStyles = hostStyles
+function bannerView (collapsed, handleCollapseAction, handleAllow, handleDeny) {
+  var containerClass = 'roboto f5-ns f6 shadow-2 bg-white pa3'
+  var toggleClass = 'fr pointer gray dim label-toggle'
+  if (collapsed) {
+    containerClass += ' collapse'
+    toggleClass += ' label-toggle--rotate'
+  }
 
-function hostStyles (selector) {
-  return {
-    visible: html`
+  var learnMore = html`
+    <a target="_blank" rel="noopener" href="https://www.offen.dev" class="normal link underline dim dark-gray">
+      ${__('Learn more')}
+    </a>
+  `
+  return html`
+    <div class="${containerClass}">
+      <p class="b mt0 mb3">
+        ${__('Continue with transparent analytics')}
+        ${collapsed ? learnMore : null}
+        <a role="button" class="${toggleClass}" onclick="${handleCollapseAction}"></a>
+      </p>
+      ${!collapsed ? html`
+        <p class="mt0 mb3">
+          ${__('Help us to make this website better by granting access to your usage data. Your data always remains yours. Review and delete it at any time.')}
+          ${learnMore}
+        </p>
+      ` : null}
+      <div class="flex">
+        <div class="w-50 mr2">
+          <button class="w-100 pointer f5 tc dim bn ph3 pv2 dib br1 white bg-dark-gray" onclick=${handleAllow}>
+            ${__('Yes Please')}
+          </button>
+        </div>
+        <div class="w-50 ml2">
+          <button class="w-100 pointer f5 tc dim bn ph3 pv2 dib br1 white bg-dark-gray" onclick=${handleDeny}>
+            ${__('I Do Not Allow')}
+          </button>
+        </div>
+      </div>
+    </div>
+  `
+}
+
+function bannerStyles () {
+  return html`
+    <style>
+      .label-toggle::after {
+        content: ' ';
+        display: inline-block;
+        border-top: 8px solid transparent;
+        border-bottom: 8px solid transparent;
+        border-left: 8px solid currentColor;
+        vertical-align: middle;
+        transform: rotate(90deg);
+        margin-right: 8px;
+      }
+      .label-toggle.label-toggle--rotate::after {
+        transform: rotate(-90deg);
+      }
+    </style>
+  `
+}
+
+function hostStylesVisible (selector, isCollapsed) {
+  var bottom = isCollapsed ? '0px' : '138px'
+  return html`
 <style>
   ${selector} {
     display: block !important;
     position: fixed;
-    bottom: 0;
+    bottom: ${bottom};
     right: 0;
     left: 0;
     margin: 0 auto;
-    height: 300px;
     width: 450px;
   }
   @media all and (max-width: 480px) {
     ${selector} {
-      height: 295px;
-      width: 100vw;
+      width: 100%;
     }
   }
-</style>`,
-    hidden: html`
+</style>
+  `
+}
+
+function hostStylesHidden (selector) {
+  return html`
 <style>
   ${selector} {
     display: none;
   }
-</style>`
-  }
+</style>
+  `
 }
 
 exports.get = getConsentStatus
