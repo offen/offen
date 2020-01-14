36d3a571a301c6f4fe7a18e03473a24266467612
Frederik Ring <frederik.ring@gmail.com>
2020-01-14 08:48:48 +0100

    fix missing package import in windows build

--- server/config/expandstring_windows.go
+++ server/config/expandstring_windows.go
@@ -2,6 +2,10 @@
 
 package config
 
+import (
+	"golang.org/x/sys/windows/registry"
+)
+
 // ExpandString expands all environment variables in the given string
 func ExpandString(s string) string {
 	r, _ := registry.ExpandString(s)
