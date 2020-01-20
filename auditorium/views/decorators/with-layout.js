85828cde28e9b0ea7aaddc9196039d382f878a58
hendr-ik <hello@niefeld.com>
2020-01-20 11:36:08 +0100

    add footer

--- auditorium/views/decorators/with-layout.js
+++ auditorium/views/decorators/with-layout.js
@@ -14,11 +14,25 @@ function withLayout (headline) {
               <h1 class="f2 normal mt0 mb4">${headline || __('Offen Auditorium')}</h1>
             </div>
           </div>
-          <div class="mw8 center ph0 ph3-ns pb5">
+          <div class="mw8 center ph0 ph3-ns pb4">
             ${flash ? html`
               <p data-role="flash-message" class="dib pa2 br2 ma0 mt3 ml3 ml0-ns bg-light-yellow">${flash}</p>
             ` : null}
             ${originalView(state, emit)}
+          </div>
+          <div class="mw8 center flex flex-column flex-row-ns justify-between ph3 pb5 moon-gray">
+            <div>
+              <p class="b ma0 mb1">Offen</p>
+              <p class="ma0 mb1">
+               ${__('Transparent web analytics')}
+              </p>
+              <p class="ma0 mb2">
+               ${__('for everyone')}
+              </p>
+            </div>
+            <div>
+              <a href="https://www.offen.dev/" class="normal link underline dim moon-gray" target="_blank">www.offen.dev</a>
+            </div>
           </div>
         </div>
       `
