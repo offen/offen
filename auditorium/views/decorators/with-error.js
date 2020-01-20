a4d7411317fbeff5f4b85070201c7f181a64ef01
hendr-ik <hello@niefeld.com>
2020-01-20 16:46:31 +0100

    try to fix circle ci error

--- auditorium/views/decorators/with-error.js
+++ auditorium/views/decorators/with-error.js
@@ -9,7 +9,7 @@ function withError () {
         // TODO: do not leak stack trace to users when NODE_ENV equals `production`
         // once things have settled
         var errorMessage = html`
-          <p class="dib pa2 br2 ma0 mt3 ml3 ml0-ns mr3 mr0-ns bg-light-yellow">${__('An error occured: %s', state.error.message)}</p>
+          <p class="error dib pa2 br2 ma0 mt3 ml3 ml0-ns mr3 mr0-ns bg-light-yellow">${__('An error occured: %s', state.error.message)}</p>
           <pre>${state.error.stack}</pre>
         `
         return errorMessage
