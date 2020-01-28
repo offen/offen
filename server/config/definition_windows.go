dacad7d8314061e411540946e2e8b30c68ab8b2f
Frederik Ring <frederik.ring@gmail.com>
2020-01-28 15:36:51 +0100

    add option to use naked port from env, use var directory for storing db

--- server/config/definition_windows.go
+++ server/config/definition_windows.go
@@ -10,6 +10,11 @@ import "time"
 type Config struct {
 	Server struct {
 		Port             int  `default:"8080"`
+		// Some runtimes (e.g. Heroku) require the usage of PORT for specifying
+		// the TCP port to bind to, no matter what. As a workaround, these environments
+		// can set OFFEN_SERVER_USENAKEDPORT to make the application use the
+		// port given by PORT.
+		UseNakedPort     bool `default:"false"`
 		ReverseProxy     bool `default:"false"`
 		SSLCertificate   EnvString
 		SSLKey           EnvString
