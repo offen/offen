bd1bc57dbcfeb6d7ed67ef15628edf6c1ed9df99
Frederik Ring <frederik.ring@gmail.com>
2020-03-25 08:10:50 +0100

    remove useless conditional

--- auditorium/src/views/auditorium-user.js
+++ auditorium/src/views/auditorium-user.js
@@ -80,7 +80,7 @@ const AuditoriumView = (props) => {
       <div class='flex flex-column flex-row-l mt4'>
         <div class='w-100 flex mb2 mr0-ns br0 br2-ns'>
           <Privacy
-            userHasOptedIn={consentStatus && consentStatus.status === 'allow'}
+            userHasOptedIn={consentStatus.status === 'allow'}
             onPurge={handlePurge}
             onConsent={expressConsent}
           />
