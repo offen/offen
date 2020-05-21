9a4089b7b89121e4474d2066dd7ce1e9a420deb5
Frederik Ring <frederik.ring@gmail.com>
2020-05-21 14:21:48 +0200

    also audit operator facing auditorium

--- integration/cypress/integration/lighthouse.spec.js
+++ integration/cypress/integration/lighthouse.spec.js
@@ -8,12 +8,22 @@ describe('Lighthouse Audits', function () {
       cy.lighthouse()
     })
   })
-  context('The auditorium', function () {
+  context('The user facing auditorium', function () {
     it('passes the lighthouse audits', function () {
       if (!Cypress.env('RUN_LIGHTHOUSE_AUDIT')) {
         this.skip()
       }
       cy.visit('/auditorium')
+      cy.lighthouse()
+    })
+  })
+  context('The auditorium', function () {
+    it('passes the lighthouse audits', function () {
+      if (!Cypress.env('RUN_LIGHTHOUSE_AUDIT')) {
+        this.skip()
+      }
+      cy.login()
+      cy.visit('/auditorium/9b63c4d8-65c0-438c-9d30-cc4b01173393/')
       cy.lighthouse()
     })
   })
