c7316859b36be401182505aac86c7c45067ad73d
Frederik Ring <frederik.ring@gmail.com>
2020-07-02 13:17:45 +0200

    split server and client deps

--- create_notice.py
+++ create_notice.py
@@ -1,25 +1,39 @@
 # Copyright 2020 - Offen Authors <hioffen@posteo.de>
 # SPDX-License-Identifier: Apache-2.0
 
-import sys
 import csv
+import argparse
+
+"""
+This script is used to automatically generate a NOTICE file from different
+csv sources. Right now, input from `npm-license-crawler` [0] (for npm managed
+dependencies) and `license_finder` (for Go modules) [1] is supported.
+
+[0]: https://www.npmjs.com/package/npm-license-crawler
+[1]: https://github.com/pivotal/LicenseFinder
+"""
 
 def normalize_row(row):
     result = {}
     try:
-        result['name'] = row['module name']
+        result["name"] = row["module name"]
     except KeyError:
-        result['name'] = row['repository']
+        result["name"] = row["repository"].rsplit("/", 1)[-1]
 
-    result['licenses'] = row['licenses']
-    result['source'] = row['repository']
+    result["licenses"] = row["licenses"]
+
+    result["source"] = (
+        row["repository"]
+        if row["repository"].startswith("http")
+        else "https://{}".format(row["repository"])
+    )
 
     return result
 
 
 def read_file(filename):
     result = []
-    with open(filename, 'r') as csv_file:
+    with open(filename, "r") as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             result.append(normalize_row(row))
@@ -29,26 +43,54 @@ def read_file(filename):
 def dedupe(dependencies):
     result = []
     for dep in dependencies:
-        if not any(existing['name'] == dep['name'] for existing in result):
+        if not any(existing["name"] == dep["name"] for existing in result):
             result.append(dep)
     return result
 
 
-def main(*sources):
-    all_deps = []
-    for source in sources:
-        all_deps += read_file(source)
-    all_deps = dedupe(all_deps)
+def main(app='Offen', server=[], client=[]):
+    client_deps = []
+    for source in client:
+        client_deps += read_file(source)
+    client_deps = dedupe(client_deps)
 
-    print("""Offen bundles the following 3rd party software:
-    """)
-    for dep in all_deps:
+    server_deps = []
+    for source in server:
+        server_deps += read_file(source)
+    server_deps = dedupe(server_deps)
+
+    print("{} bundles the following 3rd party software:".format(app))
+    if client_deps:
         print(
-            "\"{}\" licensed under {}, available at {}".format(
-                dep['name'], dep['licenses'], dep['source'],
-            )
+            """
+Client side:
+============
+"""
         )
+        for dep in client_deps:
+            print(
+                '"{}" licensed under {}, available at {}'.format(
+                    dep["name"].strip(), dep["licenses"].strip(), dep["source"].strip(),
+                )
+            )
+    if server_deps:
+        print(
+            """
+Server side:
+============
+        """
+        )
+        for dep in server_deps:
+            print(
+                '"{}" licensed under {}, available at {}'.format(
+                    dep["name"].strip(), dep["licenses"].strip(), dep["source"].strip(),
+                )
+            )
 
 
 if __name__ == "__main__":
-    main(*sys.argv[1:])
+    parser = argparse.ArgumentParser(description="Create a NOTICE file")
+    parser.add_argument("--client", dest="client", action="append", default=[])
+    parser.add_argument("--server", dest="server", action="append", default=[])
+    args = parser.parse_args()
+    main(server=args.server, client=args.client)
