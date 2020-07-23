648e593fb575cfb8ffe9a36eff359e193fcb71a0
Frederik Ring <frederik.ring@gmail.com>
2020-07-23 14:18:14 +0200

    increase test coverage

--- server/persistence/relational/events.go
+++ server/persistence/relational/events.go
@@ -98,38 +98,3 @@ func (r *relationalDAL) DeleteEvents(q interface{}) (int64, error) {
 		return 0, persistence.ErrBadQuery
 	}
 }
-
-func (r *relationalDAL) CreateTombstone(t *persistence.Tombstone) error {
-	local := importTombstone(t)
-	if err := r.db.Create(&local).Error; err != nil {
-		return fmt.Errorf("relational: error creating tombstone: %w", err)
-	}
-	return nil
-}
-
-func (r *relationalDAL) FindTombstones(q interface{}) ([]persistence.Tombstone, error) {
-	switch query := q.(type) {
-	case persistence.FindTombstonesQueryByAccounts:
-		var result []Tombstone
-		if err := r.db.Find(&result, "account_id IN (?) AND sequence > ?", query.AccountIDs, query.Since).Error; err != nil {
-			return nil, fmt.Errorf("relational: error looking up tombstones by account ids: %w", err)
-		}
-		var export []persistence.Tombstone
-		for _, t := range result {
-			export = append(export, t.export())
-		}
-		return export, nil
-	case persistence.FindTombstonesQueryBySecrets:
-		var result []Tombstone
-		if err := r.db.Find(&result, "secret_id IN (?) AND sequence > ?", query.SecretIDs, query.Since).Error; err != nil {
-			return nil, fmt.Errorf("relational: error looking up tombstones by secret ids: %w", err)
-		}
-		var export []persistence.Tombstone
-		for _, t := range result {
-			export = append(export, t.export())
-		}
-		return export, nil
-	default:
-		return nil, persistence.ErrBadQuery
-	}
-}
