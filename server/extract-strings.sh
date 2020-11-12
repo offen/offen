4c9ed81cb0833e36a4658dc074d0ee2dee3248a1
Frederik Ring <frederik.ring@gmail.com>
2020-11-12 18:51:35 +0100

    merge all message files into one
    this allows using an online tool for managing translation strings

--- server/extract-strings.sh
+++ server/extract-strings.sh
@@ -1,21 +1,8 @@
-#! /bin/bash
+#!/bin/bash
 # Copyright 2020 - Offen Authors <hioffen@posteo.de>
 # SPDX-License-Identifier: Apache-2.0
 
 set -eo pipefail
 
-locales=$(cat ./locales/LINGUAS)
-
-if [ -z "$locales" ]; then
-	echo "No locales configured. Nothing to do."
-	exit 0
-fi
-
-for locale in $locales
-do
-	touch locales/$locale.po
-	go run cmd/extract-strings/main.go public/*.go.html | xgettext -c~ --no-location --from-code=UTF-8 --language=python --output=locales/$locale-update.po -
-	msgmerge "locales/$locale.po" "locales/$locale-update.po" -o "locales/$locale.po"
-	rm "locales/$locale-update.po"
-	echo "Extracted strings for locale $locale"
-done
+go run cmd/extract-strings/main.go public/*.go.html \
+  | xgettext --omit-header --color=never -o - -c~ --no-location --language=python -
