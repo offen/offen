da58306437ae3dc4067ddfad5667411831b9d7c1
Frederik Ring <frederik.ring@gmail.com>
2020-12-03 11:48:39 +0100

    add slider component for onboarding content

--- auditorium/src/views/components/auditorium/user-onboarding.js
+++ auditorium/src/views/components/auditorium/user-onboarding.js
@@ -5,12 +5,14 @@
 
 /** @jsx h */
 const { h, Fragment } = require('preact')
+const classnames = require('classnames')
+
+const Slides = require('./slides')
 
 const UserOnboarding = (props) => {
   const { onComplete } = props
   return (
     <Fragment>
-      <h1>{__('Onboarding placeholder')}</h1>
       <a
         class='pointer'
         role='button'
@@ -18,6 +20,64 @@ const UserOnboarding = (props) => {
       >
         {__('Skip')}
       </a>
+      <Slides
+        slides={[
+          (props) => {
+            const { onNext } = props
+            return (
+              <Fragment>
+                <p>{__('Slide 1 content')}</p>
+                <button onclick={onNext}>
+                  {__('Next')}
+                </button>
+              </Fragment>
+            )
+          },
+          (props) => {
+            const { onNext } = props
+            return (
+              <Fragment>
+                <p>{__('Slide 2 content')}</p>
+                <button onclick={onNext}>
+                  {__('Next')}
+                </button>
+              </Fragment>
+            )
+          },
+          (props) => {
+            return (
+              <Fragment>
+                <p>{__('Slide 3 content')}</p>
+                <button onclick={onComplete}>
+                  {__('Open Auditorium')}
+                </button>
+              </Fragment>
+            )
+          }
+        ]}
+        navigation={(props) => {
+          const { onChange, activeItem, numItems } = props
+          return (
+            <ul>
+              {Array.from({ length: numItems }).map(function (el, index) {
+                return (
+                  <li key={`slider-item-${index}`} onclick={() => onChange(index)}>
+                    <a
+                      class={classnames({
+                        pointer: true,
+                        b: index === activeItem
+                      })}
+                      role='button'
+                    >
+                      {index + 1}
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          )
+        }}
+      />
     </Fragment>
   )
 }
