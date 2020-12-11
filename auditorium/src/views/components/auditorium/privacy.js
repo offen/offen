fdc92bc4e038be1fec5a655e24f581b1c5deaca4
Hendrik Niefeld <hello@niefeld.com>
2020-12-11 20:45:13 +0100

    clean up code, add license

--- auditorium/src/views/components/auditorium/privacy.js
+++ auditorium/src/views/components/auditorium/privacy.js
@@ -23,7 +23,6 @@ const Privacy = (props) => {
   let deleteButton = null
   if (userHasOptedIn) {
     deleteButton = (
-
       <div class='w-100 w-50-m w-25-l bl-ns b--moon-gray pl4-ns pb3 pb4-l'>
         <div class='flex flex-column-ns'>
           <Paragraph class='ma0 mb1'>
@@ -43,23 +42,15 @@ const Privacy = (props) => {
           </button>
         </div>
       </div>
-
     )
   }
 
   return (
     <div class='bg-black-05 flex-auto pa3'>
-
-
       <h4 class='f4 normal mt0 mb3'>
         {__('Privacy')}
       </h4>
-
-
-
       <div class='flex flex-column flex-wrap-m flex-row-ns'>
-
-
         <div class='w-100 w-50-m w-25-l pr3-m pr4-l pb4'>
           <div class='flex flex-column-ns'>
             <Paragraph class='ma0 mb1'>
@@ -83,17 +74,13 @@ const Privacy = (props) => {
             </button>
           </div>
         </div>
-
-
         {deleteButton}
-
-
         <div class='w-100 w-50-l bl-l b--moon-gray pl4-l mt3 mt0-l'>
           <div class='cf mb3'>
             <div class='fl mr3'>
-              <svg width='30' height='30' fill="none" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="15" cy="15" r="14.25" stroke="#555" stroke-width="1.5"/>
-                <path d="m15.905 16.186h-1.666l-0.1833-8.1862h2.0406l-0.1913 8.1862zm-1.9051 3.5029c0-0.3029 0.0983-0.5526 0.2949-0.7492 0.1966-0.202 0.465-0.3029 0.8051-0.3029s0.6085 0.1009 0.8051 0.3029c0.1966 0.1966 0.2949 0.4463 0.2949 0.7492 0 0.2923-0.0957 0.5367-0.287 0.7334-0.1913 0.1966-0.4623 0.2949-0.813 0.2949s-0.6217-0.0983-0.813-0.2949c-0.1913-0.1967-0.287-0.4411-0.287-0.7334z" fill="#333"/>
+              <svg width='30' height='30' fill='none' viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'>
+                <circle cx='15' cy='15' r='14.25' stroke='#555' stroke-width='1.5' />
+                <path d='m15.905 16.186h-1.666l-0.1833-8.1862h2.0406l-0.1913 8.1862zm-1.9051 3.5029c0-0.3029 0.0983-0.5526 0.2949-0.7492 0.1966-0.202 0.465-0.3029 0.8051-0.3029s0.6085 0.1009 0.8051 0.3029c0.1966 0.1966 0.2949 0.4463 0.2949 0.7492 0 0.2923-0.0957 0.5367-0.287 0.7334-0.1913 0.1966-0.4623 0.2949-0.813 0.2949s-0.6217-0.0983-0.813-0.2949c-0.1913-0.1967-0.287-0.4411-0.287-0.7334z' fill='#333' />
               </svg>
             </div>
             <div class='h2 flex flex-wrap items-center'>
@@ -106,11 +93,7 @@ const Privacy = (props) => {
             </div>
           </div>
         </div>
-
-
       </div>
-
-
     </div>
   )
 }
