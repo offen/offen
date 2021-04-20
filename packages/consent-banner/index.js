2a3d5dcc257aa478865ba4cc4dc70b439b6e920f
Frederik Ring <frederik.ring@gmail.com>
2021-04-20 17:44:01 +0200

    build: prepare moving consent banner to shared package

---
+++ packages/consent-banner/index.js
@@ -0,0 +1,98 @@
+/**
+ * Copyright 2021 - Offen Authors <hioffen@posteo.de>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+var html = require('nanohtml')
+var raw = require('nanohtml/raw')
+var sf = require('sheetify')
+
+sf('./index.css')
+
+exports.bannerView = function (props) {
+  return html`
+    <div class="banner__host">
+      ${props.consentGiven ? stepOne(props) : stepTwo(props)}
+    </div>
+  `
+}
+
+function stepOne (props) {
+  return html`
+    <p class="banner__paragraph banner__paragraph--first">
+      ${raw(__('Thanks for your help to make this website better.'))}
+    </p>
+    <p class="banner__paragraph">
+      ${raw(__('To manage your usage data <a class="%s"target="_blank" rel="noopener" href="%s">open the Auditorium.</a>', 'banner__anchor', '/auditorium/'))}
+    </p>
+    <div class="banner__buttons">
+      <button class="banner__button" onclick="${props.handleClose}">
+        ${__('Continue')}
+      </button>
+    </div>
+  `
+}
+
+function stepTwo (props) {
+  return html`
+    <p class="banner__paragraph banner__paragraph--first">
+      ${__('We only access usage data with your consent.')}
+    </p>
+    <p class="banner__paragraph">
+      ${__('You can opt out and delete any time.')}
+      <a class="banner__anchor" target="_blank" rel="noopener" href="/">
+        ${__('Learn more')}
+      </a>
+    </p>
+    <div class="banner__buttons">
+      <button class="banner__button banner__button--first" onclick=${props.handleAllow}>
+        ${__('I allow')}
+      </button>
+      <button class="banner__button banner__button--last" onclick=${props.handleDeny}>
+        ${__("I don't allow")}
+      </button>
+    </div>
+   `
+}
+
+exports.fontStylesheet = function (props) {
+  return html`
+    <link rel="stylesheet" href="/fonts.css" onload=${props.onload}>
+  `
+}
+
+exports.hostStylesVisible = function (props) {
+  return html`
+<style>
+  ${props.selector} {
+    display: block !important;
+    position: fixed;
+    bottom: 138px;
+    right: 0;
+    left: 0;
+    margin: 0 auto;
+    box-shadow: 0px 0px 9px 0px rgba(0,0,0,0.50);
+    z-index: 2147483647;
+    width: 410px;
+  }
+  @media all and (max-width: 414px) {
+    ${props.selector} {
+      width: 100%;
+      border-left: none;
+      border-right: none;
+      border-radius: 0;
+    }
+  }
+</style>
+  `
+}
+
+exports.hostStylesHidden = function (props) {
+  return html`
+<style>
+  ${props.selector} {
+    display: none;
+  }
+</style>
+  `
+}
--- vault/src/user-consent.js
+++ vault/src/user-consent.js
@@ -3,17 +3,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-var html = require('nanohtml')
-var raw = require('nanohtml/raw')
-var sf = require('sheetify')
-
 var cookies = require('./cookie-tools')
+
+var consentBanner = require('offen/consent-banner')
 
 var ALLOW = 'allow'
 var DENY = 'deny'
 var COOKIE_NAME = 'consent'
-
-sf('./user-consent.css')
 
 exports.ALLOW = ALLOW
 exports.DENY = DENY
@@ -24,11 +20,9 @@ function askForConsent (styleHost) {
   return new Promise(function (resolve) {
     var consentGiven = false
     var stylesReady = new Promise(function (resolve) {
-      var styleSheet = html`
-        <link rel="stylesheet" href="/fonts.css" onload={resolve}>
-      `
-      document.head.appendChild(styleSheet)
-      resolve()
+      document.head.appendChild(
+        consentBanner.fontStylesheet({ onload: resolve })
+      )
     })
 
     window.addEventListener('resize', onResize)
@@ -47,7 +41,7 @@ function askForConsent (styleHost) {
 
     function closeHandler () {
       styleHost({
-        styles: hostStylesHidden(styleHost.selector).innerHTML
+        styles: consentBanner.hostStylesHidden({ selector: styleHost.selector }).innerHTML
       })
       while (document.body.firstChild) {
         document.body.removeChild(document.body.firstChild)
@@ -71,111 +65,30 @@ function askForConsent (styleHost) {
     function render () {
       var host = document.body.querySelector('#host')
       stylesReady.then(function () {
-        var banner = bannerView(
-          consentGiven,
-          allowHandler,
-          denyHandler,
-          closeHandler
-        )
+        var banner = consentBanner.bannerView({
+          consentGiven: consentGiven,
+          handleAllow: allowHandler,
+          handleDeny: denyHandler,
+          handleClose: closeHandler
+        })
         if (host.firstChild) {
           var current = host.firstChild
           host.insertBefore(banner, current)
           adjustHostStyles(
-            hostStylesVisible(styleHost.selector).innerHTML,
+            consentBanner.hostStylesVisible({ selector: styleHost.selector }).innerHTML,
             banner.getBoundingClientRect().height
           )
           host.removeChild(current)
         } else {
           host.appendChild(banner)
           adjustHostStyles(
-            hostStylesVisible(styleHost.selector).innerHTML,
+            consentBanner.hostStylesVisible({ selector: styleHost.selector }).innerHTML,
             banner.getBoundingClientRect().height
           )
         }
       })
     }
   })
-}
-
-function bannerView (consentGiven, handleAllow, handleDeny, handleClose) {
-  var content
-  if (consentGiven) {
-    content = html`
-      <p class="banner__paragraph banner__paragraph--first">
-        ${raw(__('Thanks for your help to make this website better.'))}
-      </p>
-      <p class="banner__paragraph">
-        ${raw(__('To manage your usage data <a class="%s"target="_blank" rel="noopener" href="%s">open the Auditorium.</a>', 'banner__anchor', '/auditorium/'))}
-      </p>
-      <div class="banner__buttons">
-        <button class="banner__button" onclick="${handleClose}">
-          ${__('Continue')}
-        </button>
-      </div>
-    `
-  } else {
-    content = html`
-      <p class="banner__paragraph banner__paragraph--first">
-        ${__('We only access usage data with your consent.')}
-      </p>
-      <p class="banner__paragraph">
-        ${__('You can opt out and delete any time.')}
-        <a class="banner__anchor" target="_blank" rel="noopener" href="/">
-          ${__('Learn more')}
-        </a>
-      </p>
-      <div class="banner__buttons">
-        <button class="banner__button banner__button--first" onclick=${handleAllow}>
-          ${__('I allow')}
-        </button>
-        <button class="banner__button banner__button--last" onclick=${handleDeny}>
-          ${__('I don\'t allow')}
-        </button>
-      </div>
-    `
-  }
-
-  return html`
-    <div class="banner__host">
-      ${content}
-    </div>
-  `
-}
-
-function hostStylesVisible (selector) {
-  return html`
-<style>
-  ${selector} {
-    display: block !important;
-    position: fixed;
-    bottom: 138px;
-    right: 0;
-    left: 0;
-    margin: 0 auto;
-    box-shadow: 0px 0px 9px 0px rgba(0,0,0,0.50);
-    z-index: 2147483647;
-    width: 410px;
-  }
-  @media all and (max-width: 414px) {
-    ${selector} {
-      width: 100%;
-      border-left: none;
-      border-right: none;
-      border-radius: 0;
-    }
-  }
-</style>
-  `
-}
-
-function hostStylesHidden (selector) {
-  return html`
-<style>
-  ${selector} {
-    display: none;
-  }
-</style>
-  `
 }
 
 exports.get = getConsentStatus
