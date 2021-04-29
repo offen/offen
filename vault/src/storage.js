5e4d2f54d5174484d56fa0ad0adbca79fc0da863
Frederik Ring <frederik.ring@gmail.com>
2021-04-29 13:26:55 +0200

    vault: remove development short circuit

--- vault/src/storage.js
+++ vault/src/storage.js
@@ -342,7 +342,7 @@ function Storage (getDatabase, fallbackStore) {
 // thus disallowing users to manage their data. Instead we fall back to cookie
 // storage forcefully by wrapping certain methods in this helper.
 function bypassSafari (thunk, err) {
-  return true || /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
+  return /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
     ? dexie.Promise.reject(new dexie.OpenFailedError('Forcefully skipping IndexedDB.'))
     : thunk()
 }
