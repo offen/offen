0eef89f7823712983b1ca23c29d1d11c0921a20e
hendr-ik <hello@niefeld.com>
2021-12-17 19:41:19 +0100

    Add spanish original and test locale

--- server/config/locale.go
+++ server/config/locale.go
@@ -11,7 +11,7 @@ type Locale string
 // Decode validates and assigns l.
 func (l *Locale) Decode(s string) error {
 	switch s {
-	case "en", "de", "fr":
+	case "en", "de", "fr", "es", "eo":
 		*l = Locale(s)
 	default:
 		return fmt.Errorf("unknown or unsupported locale %s", s)
