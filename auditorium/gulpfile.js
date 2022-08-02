4a99c27f18e68a92893a10a22d18b148e258306f
Marcel Schepaniak <marcel@taliox.io>
2022-08-02 11:50:27 +0200

    WIP - formatting & placeholder value

--- auditorium/gulpfile.js
+++ auditorium/gulpfile.js
@@ -4,7 +4,6 @@
  */
 
 var fs = require('fs')
-var path = require('path');
 var gulp = require('gulp')
 var clean = require('gulp-clean')
 var browserify = require('browserify')
@@ -14,14 +13,13 @@ var buffer = require('vinyl-buffer')
 var gap = require('gulp-append-prepend')
 var Readable = require('stream').Readable
 var linguasFile = require('linguas-file')
-var crypto = require('crypto');
+var crypto = require('crypto')
 
 var pkg = require('./package.json')
 
 var defaultLocale = 'en'
-var linguas = !process.env.SKIP_LOCALES ?
-  linguasFile.parse(fs.readFileSync('./locales/LINGUAS', 'utf-8')) :
-  []
+var linguas = !process.env.SKIP_LOCALES
+  ? linguasFile.parse(fs.readFileSync('./locales/LINGUAS', 'utf-8')) : []
 
 gulp.task('clean:pre', function () {
   return gulp
@@ -42,7 +40,7 @@ gulp.task('default', gulp.series(
   }))
 ))
 
-function createLocalizedBundle(locale) {
+function createLocalizedBundle (locale) {
   var dest = './dist/' + locale + '/auditorium/'
   var scriptTask = makeScriptTask(dest, locale)
   scriptTask.displayName = 'script:' + locale
@@ -51,40 +49,37 @@ function createLocalizedBundle(locale) {
   return gulp.parallel(scriptTask, vendorTask)
 }
 
-function makeScriptTask(dest, locale) {
+function makeScriptTask (dest, locale) {
   return function () {
     var transforms = JSON.parse(JSON.stringify(pkg.browserify.transform))
     // we are setting this at process level so that it propagates to
     // dependencies that also require setting it
     process.env.LOCALE = locale
-    process.env.SCRIPT_INTEGRITY_HASH = "unkown"
+    process.env.SCRIPT_INTEGRITY_HASH = 'unkown'
     var b = browserify({
-        entries: './index.js',
-        // See: https://github.com/nikku/karma-browserify/issues/130#issuecomment-120036815
-        postFilter: function (id, file, currentPkg) {
-          if (currentPkg && currentPkg.name === pkg.name) {
-            currentPkg.browserify.transform = []
-          }
-          return true
-        },
-        transform: transforms.map(function (transform) {
-          if (transform === '@offen/l10nify' || (Array.isArray(transform) && transform[0] === '@offen/l10nify')) {
-            return ['@offen/l10nify']
-          }
-          if (transform === 'envify' || (Array.isArray(transform) && transform[0] === 'envify')) {
-            let scriptContent = fs.readFileSync('./script/index.js', 'utf8')
+      entries: './index.js',
+      // See: https://github.com/nikku/karma-browserify/issues/130#issuecomment-120036815
+      postFilter: function (id, file, currentPkg) {
+        if (currentPkg && currentPkg.name === pkg.name) {
+          currentPkg.browserify.transform = []
+        }
+        return true
+      },
+      transform: transforms.map(function (transform) {
+        if (transform === '@offen/l10nify' || (Array.isArray(transform) && transform[0] === '@offen/l10nify')) {
+          return ['@offen/l10nify']
+        }
+        if (transform === 'envify' || (Array.isArray(transform) && transform[0] === 'envify')) {
+          const scriptContent = fs.readFileSync('./script/script.js', 'utf8')
+          process.env.SCRIPT_INTEGRITY_HASH = crypto.createHash('sha256').update(scriptContent).digest('base64')
 
-            console.log(scriptContent);
-            process.env.SCRIPT_INTEGRITY_HASH = crypto.createHash('sha256').update(scriptContent).digest('base64')
-            console.log(process.env.SCRIPT_INTEGRITY_HASH)
-            
-            return ['envify', {
-              LOCALE: locale
-            }]
-          }
-          return transform
-        })
+          return ['envify', {
+            LOCALE: locale
+          }]
+        }
+        return transform
       })
+    })
       .transform('aliasify', {
         global: true
       })
@@ -114,7 +109,7 @@ function makeScriptTask(dest, locale) {
   }
 }
 
-function makeVendorTask(dest) {
+function makeVendorTask (dest) {
   return function () {
     var b = browserify()
     return b
@@ -143,7 +138,7 @@ function makeVendorTask(dest) {
 // `react-datepicker`. Handling this at build time allows us to avoid including
 // unused locales for non-English bundles. At development time, the default
 // locale (en) will be included implicitly.
-function configureDatepickerLocale(locale) {
+function configureDatepickerLocale (locale) {
   return Readable.from([`
 const { registerLocale } = require('react-datepicker')
 const locale = require('date-fns/locale/${locale}')
@@ -151,7 +146,7 @@ registerLocale('${locale}', locale)
   `])
 }
 
-function configureCountriesLocale(locale) {
+function configureCountriesLocale (locale) {
   return Readable.from([`
 const countries = require('i18n-iso-countries')
 countries.registerLocale(require('i18n-iso-countries/langs/${locale}.json'))
