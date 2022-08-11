68a7b7f6fd0f2b02e0b013a46ece4c9f2d3725d2
Frederik Ring <frederik.ring@gmail.com>
2022-08-11 11:38:39 +0200

    extension: move persistence to background script

--- extension/content-script.js
+++ extension/content-script.js
@@ -13,53 +13,54 @@
   }
   ;(document.head || document.documentElement).appendChild(s)
 
-  const db = new Database()
-
-  document.addEventListener(
-    'Offen_connectExtension', handleConnectExtensionWith(db)
-  )
-  document.addEventListener(
-    'Offen_queryExtension', handleQueryExtensionWith(db)
-  )
+  document.addEventListener('Offen_connectExtension', handleConnectExtension)
+  document.addEventListener('Offen_queryExtension', handleQueryExtension)
 })()
 
-function handleQueryExtensionWith (db) {
-  return function handleQueryExtension (evt) {
-    db.query()
-      .then(result => {
-        window.postMessage({
-          direction: 'from-content-script',
-          message: result
-        }, '*')
-      })
-      .catch(err => {
-        console.error(
-          `Failed to query list of known installs: ${err.message}.`
-        )
-      })
-  }
+function handleQueryExtension () {
+  chrome.runtime.sendMessage({
+    type: 'QUERY'
+  }, (result) => {
+    if (result.error) {
+      console.error(
+        `Failed to query list of known installs: ${result.error.message}.`
+      )
+      return
+    }
+    window.postMessage({
+      direction: 'from-content-script',
+      message: result.payload
+    }, '*')
+  })
 }
 
-function handleConnectExtensionWith (db) {
-  return function handleConnectExtension (evt) {
-    const url = new window.URL(evt.detail.url)
-    return checkScriptIntegrity(url)
-      .then((ok) => {
-        if (!ok) {
-          console.log(`Could not verify installation at "${evt.detail.url}", skipping`)
-          return
-        }
-        return db.add(url.origin)
-          .then(() => {
-            console.log(`"${evt.detail.url}"" was added to the list of known installs.`)
-          })
+function handleConnectExtension (evt) {
+  const url = new window.URL(evt.detail.url)
+  return checkScriptIntegrity(url)
+    .then((version) => {
+      if (!version) {
+        console.log(`Could not verify installation at "${evt.detail.url}", skipping`)
+        return
+      }
+
+      return new Promise((resolve, reject) => {
+        chrome.runtime.sendMessage({ type: 'ADD', payload: url.origin }, (response) => {
+          if (response.error) {
+            reject(response.error)
+            return
+          }
+          resolve(response.payload)
+        })
       })
-      .catch(err => {
-        console.error(
-          `Failed to add "${evt.detail.hostname}" to list of known installs: ${err.message}.`
-        )
-      })
-  }
+        .then(() => {
+          console.log(`"${evt.detail.url}"" was added to the list of known installs.`)
+        })
+    })
+    .catch(err => {
+      console.error(
+        `Failed to add "${evt.detail.hostname}" to list of known installs: ${err.message}.`
+      )
+    })
 }
 
 function checkScriptIntegrity (urlObj) {
@@ -68,6 +69,7 @@ function checkScriptIntegrity (urlObj) {
       .then(r => r.text())
       .then(file => file.split('\n').map(line => line.trim()).filter(Boolean))
   }
+
   function fetchCurrentChecksum () {
     const url = new window.URL(urlObj)
     url.pathname = '/script.js'
@@ -83,89 +85,27 @@ function checkScriptIntegrity (urlObj) {
           .map(x => x.toString(16).padStart(2, '0')).join('')
       })
   }
+
+  function fetchCurrentVersion () {
+    const url = new window.URL(urlObj)
+    url.pathname = '/versionz'
+    return window.fetch(url)
+      .then((res) => {
+        return res.json()
+      })
+      .then((res) => {
+        return res.revision
+      })
+  }
+
   return Promise.all([fetchCurrentChecksum(), fetchKnownChecksums()])
     .then(([checksum, checksums]) => {
       return checksums.indexOf(checksum) >= 0
     })
-}
-
-function Database () {
-  const storage = new KeyValueStorage(1, {
-    vaults: []
-  })
-
-  this.add = (domain) => {
-    return storage.get('vaults')
-      .then((result) => {
-        let withAddition = [...result, domain]
-        withAddition = withAddition.filter((domain, index, list) => {
-          return list.indexOf(domain) === index
-        })
-        return storage.set('vaults', withAddition)
-      })
-  }
-
-  this.query = () => {
-    return storage.get('vaults')
-  }
-}
-
-function KeyValueStorage (schemaVersion = 1, seed = {}) {
-  const isReady = new Promise((resolve, reject) => {
-    chrome.storage.local.get(['__schemaVersion'], (result) => {
-      if (chrome.runtime.lastError) {
-        reject(chrome.runtime.lastError)
-        return
+    .then((ok) => {
+      if (!ok) {
+        return null
       }
-
-      if (result && result.__schemaVersion && Number.isFinite(result.__schemaVersion)) {
-        resolve()
-        return
-      }
-
-      const withSchemaVersion = Object.assign(
-        {}, { __schemaVersion: schemaVersion }, seed
-      )
-      Promise.all(Object.entries(withSchemaVersion).map(([key, value]) => {
-        return new Promise((resolveOne, rejectOne) => { // eslint-disable-line promise/param-names
-          chrome.storage.local.set({ [key]: value }, () => {
-            if (chrome.runtime.lastError) {
-              rejectOne(chrome.runtime.lastError)
-              return
-            }
-            resolveOne()
-          })
-        })
-      }))
-        .then(resolve, reject)
+      return fetchCurrentVersion()
     })
-  })
-
-  this.set = (key, value) => {
-    return isReady.then(() => {
-      return new Promise((resolve, reject) => {
-        chrome.storage.local.set({ [key]: value }, () => {
-          if (chrome.runtime.lastError) {
-            reject(chrome.runtime.lastError)
-            return
-          }
-          resolve()
-        })
-      })
-    })
-  }
-
-  this.get = (key) => {
-    return isReady.then(() => {
-      return new Promise((resolve, reject) => {
-        chrome.storage.local.get([key], (result) => {
-          if (chrome.runtime.lastError) {
-            reject(chrome.runtime.lastError)
-            return
-          }
-          resolve(result[key])
-        })
-      })
-    })
-  }
 }
