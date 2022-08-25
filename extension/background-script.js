94c90079701b1c610563ddaede409ba834eaa42f
Frederik Ring <frederik.ring@gmail.com>
2022-08-25 12:02:01 +0200

    extension: add icon assets

--- extension/background-script.js
+++ extension/background-script.js
@@ -12,7 +12,7 @@ chrome.runtime.onMessage.addListener(function (message, sender, respond) {
   switch (message.type) {
     case 'ADD':
       chrome.browserAction.setIcon({
-        path: 'xxx.png',
+        path: 'on.png',
         tabId: sender.tab.id
       })
       tabs[sender.tab.id] = message.payload
