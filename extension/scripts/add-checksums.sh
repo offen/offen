babbb9e0527249e353767bf02bad27905f1b82f6
Frederik Ring <frederik.ring@gmail.com>
2022-10-22 19:31:45 +0200

    extension: add script for merging output into checksums file

--- extension/scripts/add-checksums.sh
+++ extension/scripts/add-checksums.sh
@@ -12,7 +12,6 @@ do
     while [ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:9999/script.js)" != "200" ]; do
       sleep 2
     done
-    checksum=$(curl -sSL localhost:9999/script.js | sha256sum | cut -c1-64)
-    echo $checksum
+    ./scripts/get-checksums.js http://localhost:9999 | ./scripts/merge.js ./checksums.json "$VERSION"
     docker stop offen_checksum > /dev/null 2>&1
 done
