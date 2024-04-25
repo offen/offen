be71c9d45be7a3ad730b6f23d1904a0460dd20dd
Frederik Ring <frederik.ring@gmail.com>
2024-04-25 16:56:56 +0200

    locales: scaffold polish translation

--- server/config/locale.go
+++ server/config/locale.go
@@ -11,7 +11,7 @@ type Locale string
 // Decode validates and assigns l.
 func (l *Locale) Decode(s string) error {
 	switch s {
-	case "en", "de", "fr", "es", "pt", "vi":
+	case "en", "de", "fr", "es", "pt", "vi", "po":
 		*l = Locale(s)
 	default:
 		return fmt.Errorf("unknown or unsupported locale %s", s)
